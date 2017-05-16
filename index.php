a4c1c48dacff117ce2d9e48eaee1efead29abe0a
Anathea <solene.fernandez-serrano@hetic.net>
2017-05-16 11:34:27 +0200

    New test :ok_hand:

--- index.php
+++ index.php
@@ -8,19 +8,41 @@
 
 require 'crud/DB.php';
 require 'crud/Region.php';
+require 'crud/Partenaire.php';
 
 DB::init();
 //$region = new Region();
-//$region->nom = 'chatPacha';
+//$region->nom = "chatPacha";
 //$region->create();
 //$region->read();
 //var_dump($region);
-//$region->nom = 'Pouchin !';
+//$region->nom = "Chat marche !!!";
 //$region->update();
 //$region->read();
 //var_dump($region);
-$region = new Region(30);
-$region->delete();
-$region = new Region(30);
-$region->read();
-var_dump($region);
+//$region = new Region(30);
+//$region->delete();
+//$region = new Region(30);
+//$region->read();
+//var_dump($region);
+
+//$partenaire = new Partenaire(2);
+//$partenaire->nom = "truc";
+//$partenaire->libelle = "Agencement, mobilier, métiers d'art, décoration, cheminées";
+//$partenaire->id_region = "1";
+//$partenaire->departement = "69";
+//$partenaire->site = "http://www.equip-plus.fr/";
+//$partenaire->create();
+//$partenaire->read();
+//var_dump($partenaire);
+//$partenaire->nom = "Equi'plus";
+//$partenaire->libelle = "Agencement, mobilier, métiers d'art, décoration, cheminées";
+//$partenaire->id_region = "";
+//$partenaire->departement = "68";
+//$partenaire->site = "http://www.equip-plus.fr/";
+//$partenaire->update();
+//$partenaire->read();
+//var_dump($partenaire);
+//$partenaire->delete();
+//$partenaire->read();
+//var_dump($partenaire);
